9054300c0fc77080cd8ef2b13616594fb8f1be0f
BreadsB <mchlebek323@gmail.com>
2022-06-09 23:39:27 +0200

    14.3. Spring Configuration using class

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.library;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,8 +9,14 @@ import java.util.List;
 public class Library {
 
     private final List<String> books = new ArrayList<>();
-    @Autowired
     private LibraryDbController libraryDbController;
+
+    public Library() {
+        System.out.println("Hello world");
+    }
+    public Library(final LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
+    }
 
     public void saveToDb() {
         libraryDbController.saveData();
--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -3,6 +3,10 @@ package com.kodilla.spring.library;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootTest
 public class LibraryTestSuite {
@@ -11,12 +15,19 @@ public class LibraryTestSuite {
     Library library;
 
     @Test
-    void testLoadFromDb() {
-        library.loadFromDb();
-    }
+    void testLoadFromDb() { library.loadFromDb(); }
 
     @Test
     void testSaveToDb() {
         library.saveToDb();
+    }
+
+    @Test
+    void testContext() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        System.out.println("--------");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("--------");
     }
 }
